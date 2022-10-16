e6a35fbb00486a43330fb8bd4df37b824a0f109c
CecilePerrin <cperrin.387@gmail.com>
2022-10-16 21:41:53 +0200

    adding new color and toggle with motion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import'./Navbar.scss';
 import { images } from '../../constants';
 import {motion} from 'framer-motion';
-import{ HiMenuAlt4 } from 'react-icons/hi';
+import{ HiMenuAlt4, HiX } from 'react-icons/hi';
 
 const Navbar = () => {
   const [toggle, setToggled] = useState(false);
@@ -22,6 +22,16 @@ const Navbar = () => {
         </ul>
         <div className='app__navbar-menu'>
           <HiMenuAlt4 onClick={()=>setToggled(true)}/>
+
+          {
+            toggle && (
+              <motion.div>
+                whileInView={{x:[300,0]}}
+                transition ={{duration:0.85, ease: 'easeOut'}}
+                <HiX onClick={()=> setToggled(false)}/>
+              </motion.div>
+            )
+          }
         </div>
     </nav>
   )
